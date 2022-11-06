41d3ab0583fb6046787636a030873e2250110716
francosingh5 <sifranco@fi.uba.ar>
2022-11-05 22:47:48 -0300

    Implementaciones de malla, celula, celda y class Dimenciones

--- celda.h
+++ celda.h
@@ -1,4 +1,7 @@
+using namespace std;
+
 enum Tipo{
+    estandar, //no hace nada
     contaminada,
     envenenada,
     procreadora,
@@ -10,6 +13,10 @@ enum Estado{
     vivo,
     muerto
 };
+
+int ALTO = 5;
+int LARGO = 5;
+int ANCHO = 5;
 
 
 class Gen {
@@ -93,6 +100,7 @@ class AdministradorDeGen {
 
 class Celula{
     private:
+        Estado estado;
         Gen gen1;
         Gen gen2;
         Gen gen3;
@@ -114,12 +122,21 @@ class Celula{
         void mezclarGenes();
 };
 
+//calse Celula en cpp:
+Celula::celula(){
+    this->estado = muerto;
+    this->gen1 = 0;
+    this->gen2 = 0;
+    this->gen3 = 0;
+
+}
+//
+
 class Celda{
     private:
-        Estado estado;
-        int posicionX;
-        int posicionY;
-        int posicionZ;
+        unsigned int posicionX;
+        unsigned int posicionY;
+        unsigned int posicionZ;
         Tipo tipo;
         Celula celula;
     public:
@@ -133,11 +150,142 @@ class Celda{
 
 };
 
+//clase Celda en cpp:
+
+Celda::celda(){
+    this->posicionX = 0;
+    this->posicionY = 0;
+    this->posicionZ = 0;
+    this->tipo = estandar;
+    this->celula = celula(); 
+}
+
+Celda::celda(int posicionX, int posicionY, int posicionZ, Tipo tipo){
+    this->posicionX = posicionX;
+    this->posicionY = posicionY;
+    this->posicionZ = posicionZ;
+    this->tipo = estandar;
+    this->celula = celula(); 
+}
+
+Celda::celda(int posicionX, int posicionY, int posicionZ){
+    this->posicionX = posicionX;
+    this->posicionY = posicionY;
+    this->posicionZ = posicionZ;
+    this->tipo = estandar;
+    this->celula = celula(); 
+}
+
+Celda::celda(int posicionX, int posicionY, int posicionZ, Tipo tipo; Celula celula_new){
+    this->posicionX = posicionX;
+    this->posicionY = posicionY;
+    this->posicionZ = posicionZ;
+    this->tipo = estandar;
+    this->celula = celula_new; 
+}
+
+//
 class Malla{
     private:
-        Celda matriz[125];
-        int cursor;
+        Dimenciones espacio;
+        Celda p = new matriz[MAXIMO];
+        unsigned int cursor;  // 1 2 3 -> 45(?
 
     public:
+        void crearMalla();
+        void aniadirCelula(celula newCelula);
+        int getCursor();//devuelve resultado
+        void setCursor();//calculo
 
 };
+
+
+class Dimenciones{
+    private:
+        unsigned int MAXIMO;
+        unsigned int ALTO;
+        unsigned int LARGO;
+        unsigned int ANCHO;
+
+    public:
+        Dimenciones();
+        int getAncho();
+        int getAlto();
+        int getLargo();
+        void setAncho();
+        void setLargo();
+        void setAlto();
+
+}
+
+//clase Dimenciones en cpp:
+
+Dimenciones::Dimenciones(){
+    this->MAXIMO = 25;
+    this->ALTO = 0;
+    this->LARGO = 0;
+    this->ANCHO = 0;
+}
+
+//clase MAlla en cpp:
+
+unsigned int Dimenciones::getAncho(){
+    return this->ANCHO;
+}
+
+unsigned int Dimenciones::getLargo(){
+    return this->LARGO;
+}
+
+unsigned int Dimenciones::getAlto(){
+    return this->ALTO;
+}
+
+void Dimenciones::setAncho(){
+    cout << "decime el ancho de la matriz: " << endl;
+    cin << this-> ANCHO << endl ;
+}
+
+void Dimenciones::setLargo(){
+    cout << "decime el largo de la matriz: " << endl;
+    cin << this-> LARGO << endl ;}
+
+void Dimenciones::setAlto(){
+    cout << "decime el alto de la matriz: " << endl;
+    cin << this-> ALTO << endl ;
+}
+
+
+unsigned int Malla::getCursor(){ //devuelve un int xd;
+    return this->cursor;
+}
+
+void Malla::setCursor(int numero_lista){
+    this->cursor = numero_lista;
+}
+
+void Malla::setCursor(int x, int y, int z){
+    this->cursor = ((z * getAlto() * getLargo()) + (y * getLargo()) + x);
+}
+
+void Malla::Malla(){
+    setLargo();
+    setAncho();
+    setAlto();
+    for (int pagina = 0; pagina < getAncho(); ++pagina)
+    {
+        for (int columna = 0; columna < getLargo(); ++columna)
+       {
+            for (int fila = 0; fila < getAlto(); ++fila)
+            {   
+                   matriz[ pagina*getAlto()*getLargo + columna*getLargo + fila ] = Celda(fila, columna, pagina);
+            }
+        }
+    }
+
+}
+
+
+
+
+//
